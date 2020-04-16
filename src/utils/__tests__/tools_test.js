6c859b5206c12f6e76c24ec3b57d3bf550cc95f5
chris-paul <18270890760@163.com>
2020-04-16 23:17:37 +0800

    utils测试

--- src/utils/__tests__/tools_test.js
+++ src/utils/__tests__/tools_test.js
@@ -1,32 +1,93 @@
 /*
- * @Author: your name
+ * @Author: 廉恒凯
  * @Date: 2020-01-12 06:39:48
- * @LastEditTime : 2020-01-14 11:57:53
- * @LastEditors  : Please set LastEditors
+ * @LastEditTime: 2020-04-16 23:16:35
+ * @LastEditors: 廉恒凯
  * @Description: In User Settings Edit
  * @FilePath: /react-delicacies/src/utils/tests/tools.test.js
  */
-import { symbol } from '@tests/utils/tools';
+// import { symbol, slice } from '@tests/utils/tools';
 import * as tools from '../tools';
 
-describe('isString', () => {
-    test('should return `true` for strings', () => {
-        expect(tools.isString('a')).toBe(true);
-        expect(tools.isString(Object('a'))).toBe(true);
+let undefinedVar;
+describe('tools utils', () => {
+    describe('isString', () => {
+        test.each([
+            ['a', true, '(shoule true when input is string)'],
+            [Object('a'), true, '(shoule true when input is String Object)'],
+            [1, false, '(shoule true when input is String number)'],
+            [true, false, '(shoule false when input is string)'],
+            [undefinedVar, false, '(shoule false when input is undefined)'],
+            [null, false, '(shoule false when input is null)'],
+            [[1, 2, 3], false, '(shoule false when input is Array)'],
+            [new Date(), false, '(shoule false when input is Date Object)'],
+            [new Error(), false, '(shoule false when input is Error Object)'],
+            [{ '0': 1, length: 1 }, false, '(shoule false when input is normal Object)'],
+            [() => {}, false, '(shoule false when input is function)'],
+            [/x/, false, '(shoule false when input is RegExp)'],
+            [Symbol('a'), false, '(shoule false when input is symbol)'],
+        ])(`when input is %s should return %s (%s)`, (input, expected) => {
+            expect(tools.isString(input)).toBe(expected);
+        });
     });
 
-    test('should return `false` for non-strings', () => {
-        let undefinedVar;
-        expect(tools.isString(1)).toBe(false);
-        expect(tools.isString(true)).toBe(false);
-        expect(tools.isString(undefinedVar)).toBe(false);
-        expect(tools.isString(null)).toBe(false);
-        expect(tools.isString([1, 2, 3])).toBe(false);
-        expect(tools.isString(new Date())).toBe(false);
-        expect(tools.isString(new Error())).toBe(false);
-        expect(tools.isString({ '0': 1, length: 1 })).toBe(false);
-        expect(tools.isString(Array.prototype.slice)).toBe(false);
-        expect(tools.isString(/x/)).toBe(false);
-        expect(tools.isString(symbol)).toBe(false);
+    describe('isDate', () => {
+        test.each([
+            [new Date(), true, '(shoule true when input is Date Object)'],
+            ['a', false, '(shoule false when input is string)'],
+            [1, false, '(shoule false when input is String number)'],
+            [Object('a'), false, '(shoule false when input is String Object)'],
+            [true, false, '(shoule false when input is string)'],
+            [undefinedVar, false, '(shoule false when input is undefined)'],
+            [null, false, '(shoule false when input is null)'],
+            [[1, 2, 3], false, '(shoule false when input is Array)'],
+            [new Error(), false, '(shoule false when input is Error Object)'],
+            [{ '0': 1, length: 1 }, false, '(shoule false when input is normal Object)'],
+            [() => {}, false, '(shoule false when input is function)'],
+            [/x/, false, '(shoule false when input is RegExp)'],
+            [Symbol('a'), false, '(shoule false when input is symbol)'],
+        ])(`when input is %s should return %s (%s)`, (input, expected) => {
+            expect(tools.isDate(input)).toBe(expected);
+        });
+    });
+
+    describe('isUndefined', () => {
+        test.each([
+            [undefinedVar, true, '(shoule true when input is undefined)'],
+            [new Date(), false, '(shoule false when input is Date Object)'],
+            [1, false, '(shoule false when input is String number)'],
+            ['a', false, '(shoule false when input is string)'],
+            [Object('a'), false, '(shoule false when input is String Object)'],
+            [true, false, '(shoule false when input is string)'],
+            [null, false, '(shoule false when input is null)'],
+            [[1, 2, 3], false, '(shoule false when input is Array)'],
+            [new Error(), false, '(shoule false when input is Error Object)'],
+            [{ '0': 1, length: 1 }, false, '(shoule false when input is normal Object)'],
+            [() => {}, false, '(shoule false when input is function)'],
+            [/x/, false, '(shoule false when input is RegExp)'],
+            [Symbol('a'), false, '(shoule false when input is symbol)'],
+        ])(`when input is %s should return %s (%s)`, (input, expected) => {
+            expect(tools.isUndefined(input)).toBe(expected);
+        });
+    });
+
+    describe('isNull', () => {
+        test.each([
+            [null, true, '(shoule true when input is null)'],
+            [undefinedVar, false, '(shoule false when input is undefined)'],
+            [1, false, '(shoule false when input is String number)'],
+            [new Date(), false, '(shoule false when input is Date Object)'],
+            ['a', false, '(shoule false when input is string)'],
+            [Object('a'), false, '(shoule false when input is String Object)'],
+            [true, false, '(shoule false when input is string)'],
+            [[1, 2, 3], false, '(shoule false when input is Array)'],
+            [new Error(), false, '(shoule false when input is Error Object)'],
+            [{ '0': 1, length: 1 }, false, '(shoule false when input is normal Object)'],
+            [() => {}, false, '(shoule false when input is function)'],
+            [/x/, false, '(shoule false when input is RegExp)'],
+            [Symbol('a'), false, '(shoule false when input is symbol)'],
+        ])(`when input is %s should return %s (%s)`, (input, expected) => {
+            expect(tools.isNull(input)).toBe(expected);
+        });
     });
 });
