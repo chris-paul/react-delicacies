034dcff91ea911a22d23b038765df72abe98e2c6
chris-paul <18270890760@163.com>
2020-09-12 09:38:52 +0800

    测试CI CD

--- src/containers/Work/index.js
+++ src/containers/Work/index.js
@@ -1,14 +1,18 @@
 /*
  * @Author: your name
  * @Date: 2019-12-28 10:16:47
- * @LastEditTime : 2020-01-05 19:48:00
- * @LastEditors  : Please set LastEditors
+ * @LastEditTime: 2020-09-12 09:38:12
+ * @LastEditors: 廉恒凯
  * @Description: In User Settings Edit
  * @FilePath: \react-delicacies\src\pages\Login\index.js
  */
 
 import React from 'react';
 
-const Work = () => <div data-test="work">Work</div>;
+const Work = () => (
+    <div className="App-title" data-test="work">
+        Work
+    </div>
+);
 
 export default Work;
