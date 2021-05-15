8741694d9848b496aa6f583852a073255384bf26
chris-paul <18270890760@163.com>
2021-05-15 14:24:43 +0800

    fix: 修复menu 渲染错误

--- src/components/Menu/constant.js
+++ src/components/Menu/constant.js
@@ -1,24 +1,26 @@
-/* eslint-disable react/display-name */
+/*
+ * @Author: 廉恒凯
+ * @Date: 2021-05-14 21:14:52
+ * @LastEditors: 廉恒凯
+ * @LastEditTime: 2021-05-15 10:05:50
+ * @Description: file content
+ */
+import React from 'react';
 import { HomeOutlined, ShoppingCartOutlined } from '@ant-design/icons';
 // 菜单数据
 const memuData = [
     {
-        key: 'work',
-        link: '/work',
-        name: '工作台',
-        menuIcon: () => {
-            // eslint-disable-next-line react/react-in-jsx-scope
-            return <HomeOutlined />;
-        },
-    },
-    {
         key: 'list',
         link: '/list',
-        name: '订单管理',
-        // eslint-disable-next-line react/react-in-jsx-scope
-        menuIcon: () => <ShoppingCartOutlined />,
+        name: '计时器',
+        menuIcon: <HomeOutlined />,
+    },
+    {
+        key: 'about',
+        link: '/about',
+        name: '关于',
+        menuIcon: <ShoppingCartOutlined />,
     },
 ];
 
-// eslint-disable-next-line import/prefer-default-export
-export { memuData };
+export default memuData;
